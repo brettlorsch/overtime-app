df77295d5f98dcdd7107aa5dfb117b7c752ef0b7
brett <brettlorsch@gmail.com>
2018-02-26 16:30:52 -0700

    Customize Administrate to Show Enums

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,8 +2,12 @@
 
 puts "1 User Created"
 
+AdminUser.create(email: "admin@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Admin", last_name: "Name")
+
+puts "1 Admin User created"
+
 100.times do |post|
   Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
 end
 
-puts "100 posts have been created"
+puts "100 Posts have been created"
